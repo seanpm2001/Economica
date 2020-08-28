e7c5ebd201d587bffc9acb4a17e266b6eca2dcad
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-08-27 20:40:31 -0700

    STARTUP.cpp
    STARTUP.cpp

---
+++ Clickers/Adventure_Communism_Mode/STARTUP.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Starting Adventure Communist";
+  cout << "Starting in PC/Desktop mode";
+  cout << "Data unavailable, qutting.";
+  return 0;
+}
+// End of script
