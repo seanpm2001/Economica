0495fe825329e551505318e3b831d81eb9c05f46
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-08-27 20:42:20 -0700

    STARTUPV1.cpp
    STARTUPV1.cpp

---
+++ Tappers/AdVenture_Capitalist_Mode/OldVersions/STARTUP.cpp/STARTUPV1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Starting AdVenture Capitalist";
+  cout << "Starting in Tablet/Touch mobile mode";
+  cout << "Data unavailable, qutting.";
+  return 0;
+}
+// End of script
