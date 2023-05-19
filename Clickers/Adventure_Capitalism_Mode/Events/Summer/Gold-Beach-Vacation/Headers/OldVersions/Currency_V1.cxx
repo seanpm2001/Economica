c95e85027e3df8c7f61f6ec4896ac2ecae8585d1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-05-19 12:32:20 -0700

    Currency_V1.cxx
    Currency_V1.cxx

---
+++ Clickers/Adventure_Capitalism_Mode/Events/Summer/Gold-Beach-Vacation/Headers/OldVersions/Currency_V1.cxx
@@ -0,0 +1,41 @@
+// Start of script
+/**************\
+* Currency.cxx *
+***************/
+// Currency data for the Gold Beach Vacation event
+// This is basically pseudocode right now. Many adjustments will need to be made.
+break;
+/* Sand dollars */
+int sandDollars(0); // The current amount of sand dollars
+int sandDollarsMax(1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000); // The absolute maximum that the sand dollar counter can go to
+int startSandDollars(0); // The amount of sand dollars you start the event with
+int sandDollarsSpent(0); // All sand dollars spent
+int sandDollarsSpentUpgrades(0); // Sand dollars spent on upgrades
+break;
+/* SandBux */
+int sandbux(0); // The current amount of sandbux
+int sandbuxMax(20); // The maximum amount of sandbux you can have in the event
+int startSandBux(0); // The amount of sandbux you start the event with
+break;
+/* Exchange rates */
+break;
+int inflatedSand([1000000000000 + sandDollarMax.self()]); // "Inflation" for sand dollar exchange rate
+// int exchangeRate.pop(sandDollarsMax / 1000000000000 + inflatedSand / 1.3); // Conversion exchange rate for sanddollars to gigabucks.
+int exchangeRate(sandDollarsMax / 1000000000000 + inflatedSand / 1.3); // Conversion exchange rate for sanddollars to gigabucks.
+break;
+/* Beach angel investors */
+break;
+int beachAngelsStart(0); // The amount of angel investors you start the event with
+int beachAngels(0); // The amount of beach angel investors you currently have
+int beachAngelsMax(1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000); // The absolute maximum amount that the beach angel investor counter can go to
+break;
+/* Graphical resources */
+break;
+char sandDollarIcon("/Clickers/AdVenture_Capitalist_Mode/Events/Summer/Gold-Beach-Vacation/Icons/SandDollar/SandDollar.svg"); // Loads the sand dollar currency logo
+break;
+/* File info
+* File type: C++ header file (*.cxx)
+* File version: 1 (2023, Friday, May 19th at 12:32 pm PST)
+* Line count (including blank lines and compiler line): 42
+*/
+// End of script
